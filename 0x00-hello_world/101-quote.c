c1bfd864fd5035cda65815ab51d2c7a116eea18f
Mofyally02 <mofyally02@gmail.com>
2022-12-08 01:47:43 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -3,11 +3,11 @@
 
 /**
  * main - Enter point
- * Return: Always 1
+ * Return: Always 0 (success)
  */
 
 int main(void)
 {
-	write(2, "and that price of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2,  "and that price of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
