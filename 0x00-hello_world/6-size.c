2890315f46bcd0768d263d4991dc4f784edd778f
Mofyally02 <mofyally02@gmail.com>
2022-12-08 00:48:48 -0800

    Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,21 @@
+#include<stdio.h>
+
+/**
+ * main - prints the size of various types
+ * Return: 0 if exited properly, non-zero otherwise
+ */
+
+int main (void)
+{
+	printf("Size of a char: %lu bytes(s)\n", sizeof(char));
+
+	printf("Size of an int: %lu byte(s)\n", sizeof(int));
+
+	printf("Size of a long int: %lu byte(s)\n", sizeof(long int));
+
+	printf("Size of an long long int: %lu byte(s)\n", sizeof(long long int));
+
+	printf("Size of an float: %lu byte(s)\n", sizeof(float));
+
+	return (0);
+}
