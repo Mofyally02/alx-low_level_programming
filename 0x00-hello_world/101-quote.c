9817e6724dd71b5f960b35705bfd257ee14299ab
Mofyally02 <mofyally02@gmail.com>
2022-12-08 01:58:22 -0800

    Task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -2,12 +2,11 @@
 #include <unistd.h>
 
 /**
- * main - Enter point
- * Return: Always 1 (success)
+ * main - prints exactly and that piece of art is useful
+ * Return: Always 1 (Success)
  */
-
 int main(void)
 {
-	write(2, "and that price of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
 	return (1);
 }
