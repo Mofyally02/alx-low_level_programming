93d33282b97536a889bcf1a0a9d92a5c94e71661
Mofyally02 <mofyally02@gmail.com>
2022-12-09 20:51:40 -0800

    Task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints the alphabet in lowercase,
+ * followed by a newline
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for ( ch ='a'; ch <= 'z'; ch++)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
