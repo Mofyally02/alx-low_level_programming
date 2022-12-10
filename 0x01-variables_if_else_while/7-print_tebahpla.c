bfaf4a89290e6ad037dade37ff94d4755ae88d21
Mofyally02 <mofyally02@gmail.com>
2022-12-09 21:26:57 -0800

    Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print the lowercase alphabet in reverse,
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z';ch >= 'a';ch--)
+	{
+		putchar(ch);
+	}
+	putchar('\n');
+	return (0);
+}
