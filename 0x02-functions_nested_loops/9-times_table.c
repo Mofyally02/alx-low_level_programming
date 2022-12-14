34065c3e920b6cb866ee4a37dd18ac4d4d5fa342
Mofyally02 <mofyally02@gmail.com>
2022-12-14 09:43:32 -0800

    Task 9

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ * times_table - print the 9 times table
+ */
+
+void times_table(void)
+{
+	int num, mul, prod;
+
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
+
+		for (mul = 1; mul <= 9; mul++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mul;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
+}
