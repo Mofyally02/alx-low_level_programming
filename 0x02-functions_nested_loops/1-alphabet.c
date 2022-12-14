b5395ad8c2d488adda9ca0b67aa4cb8cc94851bf
Mofyally02 <mofyally02@gmail.com>
2022-12-14 09:15:53 -0800

    Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_alphabet - prints the alphabet, in lowercase, followed by a newline
+ * Return: Always 0 (success)
+ */
+
+void print_alphabet(void)
+{
+	char ch;
+
+	ch = 'a';
+
+	while (ch <= 'z')
+	{
+		_putchar(ch);
+		ch++;
+	}
+	_putchar('\n');
+}
