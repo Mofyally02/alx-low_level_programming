94146b7c133115b1d9e4962a41a1bee646a19c4f
Mofyally02 <mofyally02@gmail.com>
2022-12-15 01:06:45 -0800

    Task 0

---
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -0,0 +1,12 @@
+#include <unistd.h>
+#include "main.h"
+
+/**
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return:On success l else -l 
+ */
+int _putchar(char c)
+{
+	return (write(l, &c, l));
+}
