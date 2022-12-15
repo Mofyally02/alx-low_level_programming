d724f1f1c545c97ea3638c46735cc8bad5b0d22b
Mofyally02 <mofyally02@gmail.com>
2022-12-15 01:56:27 -0800

    Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -10,12 +10,14 @@ void more_numbers(void)
 
 	for (i = 1; i <= 9; i++)
 	{
-		for (j = 0; j <= 14; j++)
-		{
-		         if  (j > 9)
-			            _putchar('1');
-			 _putchar ((j % 10) + '0');
-		}		 
-		_putchar('\n');
+	for (j = 0; j <= 14; j++)
+	{
+	if (n > 9)
+	{
+	_putchar((n/10) + '0');
+	}
+	_putchar((n % 10) + '0');
+	}
+	_putchar('\n');
 	}
 }
