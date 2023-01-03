350b054766502d9547a5216f0e4d1409e8eac581
Mofyally02 <mofyally02@gmail.com>
2023-01-03 06:46:02 -0800

    Task 3

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,5 +1,4 @@
 #include "main.h"
-#include <stdio.h>
 
 /**
  *  _strchr - locate a character in a string
@@ -8,21 +7,24 @@
  *  Return: a pointer to the first occurrence of the character c
  *  in the string s, or NULL if the character is not found
  */
-char *_strchr(char *s, char c)
+unsigned int _strspn(char *s, char *accept)
 {
-	int a;
+	unsigned int i, j, bool;
 
-	while (1)
+	for (i = 0; *(s + i) != '\0'; i++)
 	{
-
-		a = *s++;
-		if (a == c)
+		bool = 1;
+		for (j = 0; *(accept + j) != '\0'; j++)
 		{
-			return (s - 1);
+			if (*(s + i) == *(accept + j))
+			{
+				bool = 0;
+				break;
+			}
 		}
-		if (a == 0)
-		{
-			return (NULL);
-		}
+		if (bool == 1)
+			break;
 	}
+	return (i);
 }
+
