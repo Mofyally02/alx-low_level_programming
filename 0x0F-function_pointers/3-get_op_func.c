87d726d8948f5f0ef5871c0ca264d9bf8e353d24
Mofyally02 <mofyally02@gmail.com>
2023-01-19 02:23:48 -0800

    0x0B. C - malloc, free

--- 0x0F-function_pointers/3-get_op_func.c
+++ 0x0F-function_pointers/3-get_op_func.c
@@ -9,7 +9,7 @@
  */
 int (*get_op_func(char *s))(int, int)
 {
-	op_t ops[] = {
+	op_t  ops[] = {
 		{"+", op_add},
 		{"-", op_sub},
 		{"*", op_mul},
@@ -27,6 +27,5 @@ int (*get_op_func(char *s))(int, int)
 			return (ops[i].f);
 		i++;
 	}
-
 	return (NULL);
-} 
+}
