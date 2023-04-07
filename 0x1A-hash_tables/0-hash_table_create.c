0cd0aa2907846c0c001488da67d7239d90a3ae45
Mofyally02 <mofyally02@gmail.com>
2023-04-07 01:33:08 -0700

    Task Update

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,11 +1,11 @@
-#include <stdlib.h>
 #include "hash_tables.h"
+
 /**
- * hash_table_create - creates a hash table
- * @size: size of the array
+ * hash_table_create - Creates a hash table.
+ * @size: The size of the array.
  *
- * Return: pointer to the newly created hash table or NULL
- * if something went wrong
+ * Return: If an error occurs - NULL.
+ *         Otherwise - a pointer to the new hash table.
  */
 hash_table_t *hash_table_create(unsigned long int size)
 {
--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,20 +1,19 @@
 #include "hash_tables.h"
 
 /**
- * key_index - Returns the index of a key
- * @key: The key to get the index for
- * @size: The size of the hash table's array
+ * hash_djb2 - Hash function implementing the djb2 algorithm.
+ * @str: The string to hash.
  *
- * Return: The index of the key/value pair in the hash table's array
+ * Return: The calculated hash.
  */
 unsigned long int hash_djb2(const unsigned char *str)
 {
-        unsigned long int hash;
-        int c;
+	unsigned long int hash;
+	int c;
 
-        hash = 5381;
-        while ((c = *str++))
-                hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
+	hash = 5381;
+	while ((c = *str++))
+		hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
 
-        return (hash);
+	return (hash);
 }
