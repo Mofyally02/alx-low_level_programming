fb3c07c3af94dc5efe71b854aea05f13537a6a15
Mofyally02 <mofyally02@gmail.com>
2023-04-06 21:30:32 -0700

    Update

--- 0x1A-hash_tables/1-djb2.c
+++ 0x1A-hash_tables/1-djb2.c
@@ -1,14 +1,17 @@
 #include "hash_tables.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
 
-unsigned long int hash_djb2(const unsigned char *str)
+usigned long int hash_djb2(const unsigned char *str)
 {
-    unsigned long int hash;
-    int c;
+	unsigned long int hash;
+	int c;
 
-    hash = 5381;
-    while ((c = *str++))
-    {
-        hash = ((hash << 5) + hash) + c; /* hash * 33 + c */
-    }
-    return (hash);
+	hash = 5381;
+	while ((c = *str++))
+	{
+		hash = ((hash << 5) + hash) + c;/* hash * 33 + c */
+	}
+	return (hash);
 }
---
+++ 0x1A-hash_tables/2-key_index.c
@@ -0,0 +1,20 @@
+#include "hash_tables.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+
+/**
+ * key_index - Returns the index of a key
+ * @key: The key to get the index for
+ * @size: The size of the hash table's array
+ *
+ * Return: The index of the key/value pair in the hash table's array
+ */
+unsigned long int key_index(const unsigned char *key, unsigned long int size)
+{
+	unsigned long int hash_value;
+
+	hash_value = hash_djb2(key);
+
+	return (hash_value % size);
+}
